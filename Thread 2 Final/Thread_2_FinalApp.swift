16c8edacc500d8b4bcf679d8f22b6f22a3a9bf60
Calum Couch <ccouch@lcs.on.ca>
2023-02-07 08:47:13 -0500

    Initial Commit

---
+++ Thread 2 Final/Thread_2_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thread_2_FinalApp.swift
+//  Thread 2 Final
+//
+//  Created by calum couch on 2023-02-07.
+//
+
+import SwiftUI
+
+@main
+struct Thread_2_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
